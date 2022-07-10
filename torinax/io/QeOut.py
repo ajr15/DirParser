ae2c2e99208e1421969146d6d621de5bc5040e4e
ajr15 <shaharfite@gmail.com>
2022-07-10 17:43:03 +0300

    adding QeOut 10072022

---
+++ torinax/io/QeOut.py
@@ -0,0 +1,81 @@
+from .FileParser import FileParser
+from ..Base.Structure import Structure
+from ..Base.Lattice import Lattice
+from ..Base.Atom import Atom
+import numpy as np
+
+class QeOut (FileParser):
+
+	extension = "out"
+
+
+    def read_scalar_data(self) -> dict:
+        """Reads scalar data from file to a dictionary"""
+        res = {}
+        with open(filename, "r") as f:
+            for line in f.readlines():
+                if "!" in line and 'total energy' in line:
+                    # take the last "total energy line"
+                    res['total_energy'] = float(re.findall(r'[\d|.|,|-]+', line)[0])
+        return res
+
+
+    def read_specie(self) -> Structure:
+        """Method to read Structure from the file"""
+        with open(filename, "r") as f:
+            FinalCoordsBlock = False
+            CartesianBlock = False
+            CellParamsBlock = False
+            AtomPosBlock = False
+            CoordsAreCartesian = False
+            atoms = []
+            cell_vectors = []
+            for line in f.readlines():
+                if FinalCoordsBlock is True:
+                    if len(line) > 1 and not 'End' in line:
+                        specie = re.findall(r'[A-Za-z]+', line)[0]
+                        coords = np.array([float(c) for c in re.findall(r'[\d|.|,|-]+', line)[:3]])
+                        atoms.append(Atom(specie, coords))
+                    else:
+                        FinalCoordsBlock = False
+                if 'celldm(1)' in line:
+                    param_a = float(re.findall(r'[\d|.|,|-]+', line)[1]) * 0.529177 # correction (for some reason the a parameter is scaled) TODO: FIND OUT WHY
+                if CellParamsBlock is True:
+                    if len(line) > 1:
+                        vec = np.array([float(c) * param_a for c in re.findall(r'[\d|.|,|-]+', line)[1:]])
+                        cell_vectors.append(vec)
+                    else:
+                        CellParamsBlock = False
+                if CartesianBlock:
+                    if len(line) > 1:
+                        if not 'site' in line:
+                            vec = [w for w in line.split() if not w == '']
+                            atoms.append(Atom(vec[1], np.array([float(c) for c in vec[6:9]])))
+                    else:
+                        if blank_line_counter > 0:
+                            blank_line_counter -= 1
+                        else:
+                            CartesianBlock = False
+                if "crystal axes" in line :
+                  CellParamsBlock = True
+                  cell_vectors = []
+                if 'ATOMIC_POSITIONS' in line:
+                    FinalCoordsBlock = True
+                    CoordsAreCartesian = False
+                    atoms = []
+                if "Cartesian axes" in line:
+                    CartesianBlock = True
+                    CoordsAreCartesian = True
+                    atoms = []
+                    blank_line_counter = 1
+            if CoordsAreCartesian:
+                inverse_mat = np.linalg.inv(np.array(cell_vectors))
+                for atom in atoms:
+                    atom.coordinates = np.dot(inverse_mat, atom.coordinates) * param_a
+            lat = Lattice(cell_vectors)
+            return Structure(atoms, lat)
+
+
+    def write_file(self, specie: Specie, kwdict: dict):
+        """Method to write the file type, given keywords dictionary and a specie."""
+        raise NotImplementedError("Write file method is not implemented for this file")
